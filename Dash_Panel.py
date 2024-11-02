e945bf08fa2a2f9860b8ab884eb5310e55a64d11
ShawnZyx01 <143845324+ShawnZyx01@users.noreply.github.com>
2024-11-02 18:13:02 -0500

    Update Dash_Panel.py

--- Dash_Panel.py
+++ Dash_Panel.py
@@ -120,4 +120,4 @@ tabs = pn.Tabs(
     ("Model", pn.Column(header, model_dashboard)),
 
 )
-pn.serve(tabs, port=8000)
+pn.serve(tabs, port=8000, address="0.0.0.0", allow_websocket_origin=["mesi-dash-demo.onrender.com"])
